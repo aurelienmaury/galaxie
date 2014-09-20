ea1bd2fe861108f3ce6bcc1beeb117faec27b094
Tuux <tuxa@galaxie.eu.org>
2014-09-20 02:28:35 +0200

    galaxie entropy

--- make_server.sh
+++ make_server.sh
@@ -4,6 +4,8 @@ ansible-playbook galaxie_ssh.yml
 ansible-playbook galaxie_motd.yml
 ansible-playbook galaxie_ntp.yml
 ansible-playbook install-packages-cli.yml
+ansible-playbook galaxie_bashrc.yml
 ansible-playbook galaxie_setting_vim.yml
 ansible-playbook galaxie_dvbhdhomerun.yml
 ansible-playbook galaxie_tvheadend.yml
+ansible-playbook galaxie_zfs.yml
