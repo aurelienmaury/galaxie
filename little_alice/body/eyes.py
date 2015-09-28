63cc37a79fe86c404f4eb732a47fed96f07c4635
Tuux <tuxa@rtnp.org>
2015-09-28 21:23:47 +0200

    minor change on text

--- little_alice/body/eyes.py
+++ little_alice/body/eyes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# It script it publish under GNU GENERAL PUBLIC LICENSE
+# It script it published under GNU GENERAL PUBLIC LICENSE
 # http://www.gnu.org/licenses/gpl-3.0.en.html
 # Author: Jérôme ORNECH alias "Tuux" <tuxa@rtnp.org> all rights reserved
 __author__ = 'Tuux'
@@ -45,9 +45,6 @@ class Eyes(object):
                 ret, frame = self.video_capture.read()
 
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-
-
 
                 # Draw a rectangle around the eyes
                 eyes = self.eyesCascade.detectMultiScale(
