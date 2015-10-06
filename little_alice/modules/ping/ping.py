440002106fc2b832cf9d5a21416365509ca0aab4
Tuux <tuxa@rtnp.org>
2015-10-06 20:09:13 +0200

    Add Ping Module

---
+++ little_alice/modules/ping/ping.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# It script it published under GNU GENERAL PUBLIC LICENSE
+# http://www.gnu.org/licenses/gpl-3.0.en.html
+# Author: Jérôme ORNECH alias "Tuux" <tuxa@rtnp.org> all rights reserved
+__author__ = 'Tuux'
+
