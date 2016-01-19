8a44f0dbbd5f58e13093cf84b375e5a39037e163
A. Maury <aurelien.maury+localgit@wescale.fr>
2016-01-19 13:18:03 +0100

    oeuvre d'art dhclient

---
+++ playbooks/roles/dnscache-client/files/dhclient_rm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Params:
+#   $1 - dhclient.conf file path
+#   $2 - configuration key to target
+#   $3 - configuration value to add in the value list fo corresponding key
+# Example:
+#   dhclient_add.sh /etc/dhcp/dhclient.conf request domain-name
+# Exit status:
+#   0 - file modified
+#   1 - file unchanged (either key absent or value already present)
+#   2 - error (detected and throwed by sed)
+sed  -r ":a;N;\$!ba; /$2([^;]|\n)*;/ {/$3, / {s/($2([^;]|\n)*)($3, )(([^;]|\n)*);/\1\4;/g;q}};/$2([^;]|\n)*;/ {/, $3;/ {s/($2([^;]|\n)*)(, $3;)/\1;/g;q}};q1" -i $1
